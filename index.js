97e77b5f4d1fe16e0d81dc1a048bc1750d088ba9
daniela <danielavento25@gmail.com>
2022-05-17 09:17:40 -0400

    agregando prompt

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-import {sayHello} from './mylib.js'
-
-sayHello()
+import promptFact from "prompt-sync";
+var prompt= promptFact();
+var name = prompt('Cual es su nombre: ');
+var edad = prompt('Cual es su edad: ');
+console.log(name);
+console.log(edad);
